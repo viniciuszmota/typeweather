080765e94e4ecf45364b3c200db476b6faacf448
vinicius <vinicius.quimica@gmail.com>
2024-11-22 11:07:36 -0300

    3: typescript implement

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,26 +2,27 @@ import "./styles.css"
 
 import { useNavigate } from "react-router-dom"
 import { SelectCity } from "../../components/SelectCity"
+import { CityProps } from "../../services/getCityByNameService"
 
 export function Search() {
   const navigate = useNavigate()
 
-  function handleSelect(city) {
-    localStorage.setItem("@typeweather:city", JSON.stringify(city))
+  function handleSelected(city: CityProps) {
+    localStorage.setItem("@typewheather:city", JSON.stringify(city))
     navigate("/dashboard")
   }
 
   return (
     <div className="home">
-      <img src="logo_large.svg" alt="logo" />
+      <img src="logo_large.svg" alt="Logo" />
 
       <main>
         <h1>
           Boas vindas ao <strong>TypeWeather</strong>
         </h1>
-        <span>Escolha o local para ver a previsão do tempo</span>
+        <span>Escolha um local para ver a previsão do tempo</span>
 
-        <SelectCity onSelect={handleSelect} />
+        <SelectCity onSelect={handleSelected} />
       </main>
     </div>
   )
